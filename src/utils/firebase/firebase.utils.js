9e5dafe5591c0832ea885ab87fc0c13f4b2c578b
SHEERSHIKA DHONDI <sheershika123@gmail.com>
2022-09-08 23:48:50 +0530

    upto netlify

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,49 +1,78 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
-import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc ,collection,writeBatch} from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyDTL3ol9TyQBxdhgVO4MHlA67COm15V3ug",
-    authDomain: "crwm-clothing-db-acdbe.firebaseapp.com",
-    projectId: "crwm-clothing-db-acdbe",
-    storageBucket: "crwm-clothing-db-acdbe.appspot.com",
-    messagingSenderId: "29145648953",
-    appId: "1:29145648953:web:f3eea5d6c9e70383a2e2e8"
+  apiKey: "AIzaSyDTL3ol9TyQBxdhgVO4MHlA67COm15V3ug",
+  authDomain: "crwm-clothing-db-acdbe.firebaseapp.com",
+  projectId: "crwm-clothing-db-acdbe",
+  storageBucket: "crwm-clothing-db-acdbe.appspot.com",
+  messagingSenderId: "29145648953",
+  appId: "1:29145648953:web:f3eea5d6c9e70383a2e2e8"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: 'select_account',
+});
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+export const db = getFirestore();
+
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const provider=new GoogleAuthProvider();
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-provider.setCustomParameters({
-    prompt: "select_account"
-});
-export const auth=getAuth();
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
 
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
 
-export const signInWithGooglePopup=()=>signInWithPopup(auth,provider);
+  return await signInWithEmailAndPassword(auth, email, password);
+};
 
-export const db=getFirestore();
-export const createUserDocumentFromAuth=async (userAuth)=>{
-    const userDocRef=doc(db,'users',userAuth.uid);
+export const signOutUser = async () => await signOut(auth);
 
-    console.log(userDocRef);
-
-    const userSnapshot =await getDoc(userDocRef);
-    console.log(userSnapshot);
-    console.log(userSnapshot.exists());
-
-    if(!userSnapshot.exists()){
-        const {displayName,email} = userAuth;
-        const createdAt=new Date();
-
-        try{
-            await setDoc(userDocRef,{displayName,email,createdAt,});
-        }
-        catch(error){
-            console.log('error creating the user',error.message);
-        }
-    }
-
-return userDocRef;
-}
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
