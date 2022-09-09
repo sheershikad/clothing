3d51d78e9abcab473583a70d6786f758bce03b19
SHEERSHIKA DHONDI <sheershika123@gmail.com>
2022-09-09 13:02:02 +0530

    resolving error

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -20,6 +20,25 @@ import {
   getDocs,
 } from 'firebase/firestore';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDTL3ol9TyQBxdhgVO4MHlA67COm15V3ug",
+//   authDomain: "crwm-clothing-db-acdbe.firebaseapp.com",
+//   projectId: "crwm-clothing-db-acdbe",
+//   storageBucket: "crwm-clothing-db-acdbe.appspot.com",
+//   messagingSenderId: "29145648953",
+//   appId: "1:29145648953:web:f3eea5d6c9e70383a2e2e8"
+// };
+
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
+//   authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
+//   projectId: 'crwn-clothing-db-98d4d',
+//   storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
+//   messagingSenderId: '626766232035',
+//   appId: '1:626766232035:web:506621582dab103a4d08d6',
+  
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyDTL3ol9TyQBxdhgVO4MHlA67COm15V3ug",
   authDomain: "crwm-clothing-db-acdbe.firebaseapp.com",
